7a4d6aa0ee8b7f7daeda3bc296c8ba176f162c37
Bibek Subedi <95415713+BibekSubedi5@users.noreply.github.com>
2024-09-27 18:14:07 +0545

    environment variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-
+import Config from './config/Config'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
+ 
   return (
     <div className='bg-black flex justify-center items-center w-full h-screen'>
   <h1 className='text-red-700 '>hello world</h1>
