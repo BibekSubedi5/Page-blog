c6d5495d1fdceeb2f26a92c1ec01a00e9e57b019
Bibek Subedi <95415713+BibekSubedi5@users.noreply.github.com>
2024-10-02 19:49:47 +0545

    feat:pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,38 @@
-import { useEffect, useState } from 'react';
-import Config from './config/Config';
-import './App.css';
-import { useDispatch } from 'react-redux';
-import authService from './appwrite/auth';
-import { login, logout } from './store/AuthSlice';
-import { Header,Footer } from './components/index';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import './App.css'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
   useEffect(() => {
     authService.getCurrentUser()
-      .then((userData) => {
-        if (userData) {
-          dispatch(login({ userData }));
-        } else {
-          dispatch(logout());
-        }
-      })
-      .catch((error) => {
-        console.error('Error fetching current user:', error);
-        dispatch(logout());
-      })
-      .finally(() => setLoading(false));
-  }, [dispatch]);
-
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
   return !loading ? (
-    <BrowserRouter >
-      <div className="bg-gray-500 h-screen w-full flex  flex-col gap-8 text-white">
-        <Routes>
-          <Route path="/" element={<Header />} />
-       
-        </Routes>
-        <Footer  />
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  <Outlet />
+        </main>
+        <Footer />
       </div>
-    </BrowserRouter>
-  ) : (
-    <div>Loading...</div>
-  );
+    </div>
+  ) : null
 }
 
-export default App;
+export default App
--- src/components/index.js
+++ src/components/index.js
@@ -1,21 +1,30 @@
-import Header from "./header/header";
-import Footer from "./footer/Footer"
-import Logo from "./Logo";
-import LogoutBtn from "./header/LogoutBtn";
+import Select from "./Select";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./SignUp";
+import Login from "./Login";
 import Button from "./Button";
-import Input from "./Input"
-
-
-
-
-export  {
+import PostForm from "./postform/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+export {
     Header,
     Footer,
+    Container,
     Logo,
     LogoutBtn,
-    Container,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout, 
     Input,
-    Button
-
-    }
+    Select,
+}
